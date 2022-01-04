bb37bc5affbd77719e896cf2760c4a0add151581
ErranderLee <dre12am14@gmail.com>
2022-01-04 22:51:56 +0900

    1/4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const dotenv = require('dotenv');
 
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.urlencoded({ extended: true }));
